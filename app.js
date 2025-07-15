3783422574dde56b86a3433ca3d0bf3af66ab901
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-15 19:12:24 +0200

    randomize numbers on GET /bounce

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ const logger = require('pino')()
 const app = express()
 const port = 3000
 
+const NEXT_NODE = process.env.NEXT_NODE || 'http://localhost:3000'
+
+function performMagic(package) {
+    logger.info('performing magic on package: %s', package)
+    const randomNumber = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    return randomNumber;
+}
+
 app.get('/', (req, res) => {
     logger.info('hello world')
     res.send('Hello World!')
+})
+
+app.get('/bounce/:package', (req, res) => {
+    logger.info('GET: /bounce/%s', req.params.package)
+    const newPackage = performMagic(req.params.package)
+    logger.info('new package: %s', newPackage)
+    res.send(`ok`)
 })
 
 app.listen(port, () => {
