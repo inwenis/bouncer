575711fee4cea32ac68bbd6b77819b7cfef5ca81
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-15 18:51:24 +0200

    use pino

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const logger = require('pino')()
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    logger.info('hello world')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
