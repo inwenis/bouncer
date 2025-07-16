ec59109d17901c2a707ce190d41aa689e26e9e75
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-16 11:29:31 +0200

    display current package

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 3000
 
 const NEXT_NODE = process.env.NEXT_NODE || 'http://localhost:3000'
 
+let currentPackage = null
+
 function performMagic(package) {
     logger.info('performing magic on package: %s', package)
     const randomNumber = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
@@ -15,15 +17,17 @@ function performMagic(package) {
 
 app.get('/', (req, res) => {
     logger.info('GET: /')
-    res.send('Hello World!')
+    res.send(`Current package: ${currentPackage}`)
 })
 
 app.get('/bounce/:package', (req, res) => {
+    currentPackage = req.params.package
     logger.info('GET: /bounce/%s', req.params.package)
     const newPackage = performMagic(req.params.package)
     logger.info('new package: %s', newPackage)
     // do not await the call as we do not care about the response
     // we just want to send a request to the next node
+    currentPackage = newPackage
     axios.get(`${NEXT_NODE}/bounce/${newPackage}`)
     res.send(`ok`)
 })
