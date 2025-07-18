6907423aca70add086060f71044580dd6f742d63
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-18 08:38:49 +0200

    add missing require

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const logger = require('pino')()
 const axios = require('axios')
+const { AxiosError } = require('axios')
 
 const app = express()
 const port = process.env.PORT || 3000
