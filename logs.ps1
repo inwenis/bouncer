a93deeec71007d4292f9f2e2150c4608d9a31870
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-26 10:02:04 +0200

    fix mojibake in logs

--- logs.ps1
+++ logs.ps1
@@ -8,6 +8,16 @@ function Remove-TerminalControlCharacters {
         $Line -replace '\x1b\[[0-9;]*m|\x1b\(B',''
     }
 }
+
+# By default PowerShell captures bytes and interprets them as a string using [Console]::OutputEncoding
+# which by default (for me) is code page 850. This means that if the external commands outputs UTF-8 characters,
+# some will be misinterpreted (like emojis).
+# To avoid this, we set the output encoding to UTF-8 before running the commands.
+$currentEncoding = [Console]::OutputEncoding
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
 render logs -o text -r bouncer-1 | Remove-TerminalControlCharacters > logs-1.log
 render logs -o text -r bouncer-2 | Remove-TerminalControlCharacters > logs-2.log
 render logs -o text -r bouncer-3 | Remove-TerminalControlCharacters > logs-3.log
+
+[Console]::OutputEncoding = $currentEncoding
