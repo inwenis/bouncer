b91b33ae231cbbb8f8a495f12c9a1790b1d2079c
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-07-28 09:21:37 +0200

    better format

--- fix-logs.ps1
+++ fix-logs.ps1
@@ -4,6 +4,6 @@ $lines | ForEach-Object {
     $match = $_ | Select-String -pattern "^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})"
     $dateAsStr = $match.Matches.Groups[1].Value
     $dateParsed =[System.DateTimeOffset]::Parse($dateAsStr, $null, [System.Globalization.DateTimeStyles]::AssumeUniversal)
-    $dateLocal = $dateParsed.ToLocalTime().ToString()
+    $dateLocal = $dateParsed.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss")
     $_ -replace $dateAsStr, $dateLocal
 }
