49caeaa6ba772d4d596de33bc55d4ca789ad070b
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-08-04 17:52:12 +0200

    typo

--- logs.ps1
+++ logs.ps1
@@ -25,7 +25,7 @@ function Update-TimeZone {
 }
 
 # By default PowerShell captures bytes and interprets them as a string using [Console]::OutputEncoding
-# which by default (for me) is code page 850. This means that if the external commands outputs UTF-8 characters,
+# which by default (for me) is code page 850. This means that if the external commands output UTF-8 characters,
 # some will be misinterpreted (like emojis).
 # To avoid this, we set the output encoding to UTF-8 before running the commands.
 $currentEncoding = [Console]::OutputEncoding
